03dc83d8216a382780be42e7184d303922cb35e5
Ayush Bansal <ayushbansal1928@gmail.com>
2024-06-01 22:38:53 +0530

    Changed credentials.json to env

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const fs = require("fs").promises;
 const express = require("express");
-const path = require("path");
 const process = require("process");
-const { authenticate } = require("@google-cloud/local-auth");
 const { google } = require("googleapis");
-const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 const spreadsheetId = "1AoogFvO4OdcEbz-g7fmCD1Yy8kPad47ghRRVC2bZalY";
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,8 +12,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const credentials = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
+
 const auth = new google.auth.GoogleAuth({
-  keyFile: "./credentials.json",
+  credentials: credentials,
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
